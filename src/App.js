fe1f5b4dfe673d2721d575da049232ff95263915
Yanes3 <cye13766@hotmail.com>
2024-04-07 09:02:41 -0500

    Se realiza visual de screams creados.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 import Navbar from "./components/navbar";
 
@@ -8,21 +10,44 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 class App extends Component {
   render() {
     return (
+     <MuiThemeProvider theme={theme}>
       <div className="App">
         <Router>
-        <div className = "container">
-            <Navbar/>
+          <Navbar />
+          <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
             </Switch>
-        </div>
+          </div>
         </Router>
       </div>
+     </MuiThemeProvider>
     );
   }
 }

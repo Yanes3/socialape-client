4a8fe27634c801f8f92c4e497163095980f9db97
Yanes3 <cye13766@hotmail.com>
2024-04-08 02:48:53 -0500

    Se añade boton para eliminar Comentario.

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,32 +1,48 @@
-  import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM } from '../types';
+import {
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  DELETE_SCREAM,
+} from "../types";
 
-  const initialState = {
-    screams: [],
-    scream: {},
-    loading: false
+const initialState = {
+  screams: [],
+  scream: {},
+  loading: false,
 };
 
-export default function(state = initialState, action){
-    switch(action.type){
-      case LOADING_DATA:
-        return {
-          ...state,
-          loading: true
-        };
-        case SET_SCREAMS:
-            return {
-                ...state,
-                screams: action.payload,
-                loading: false
-            };
-        case LIKE_SCREAM:
-        case UNLIKE_SCREAM:
-            let index = state.screams.findIndex((scream) => scream.screamId === action.payload.screamId);
-            state.screams[index] = action.payload;
-            return {
-                ...state
-            };
-        default:
-            return state;      
-    }
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOADING_DATA:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SET_SCREAMS:
+      return {
+        ...state,
+        screams: action.payload,
+        loading: false,
+      };
+    case LIKE_SCREAM:
+    case UNLIKE_SCREAM:
+      let index = state.screams.findIndex(
+        (scream) => scream.screamId === action.payload.screamId
+      );
+      state.screams[index] = action.payload;
+      return {
+        ...state,
+      };
+    case DELETE_SCREAM:
+      index = state.screams.findIndex(
+        (scream) => scream.screamId === action.payload
+      );
+      state.screams.splice(index, 1);
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
 }

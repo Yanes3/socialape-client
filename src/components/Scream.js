a207d9f1ee0dd0b8e9d396e10e7acb3743247f9e
Yanes3 <cye13766@hotmail.com>
2024-04-08 02:00:18 -0500

    Commentarios

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -5,16 +5,16 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
-// MUI Stuff
+
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-// Icons
+
 import ChatIcon from '@material-ui/icons/Chat';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-// Redux
+
 import { connect } from 'react-redux';
 import { likeScream, unlikeScream } from '../redux/actions/dataActions';
 

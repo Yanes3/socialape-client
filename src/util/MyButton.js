8cbbcbb95fb68563cac5ea09fc30d5b5e3066f25
Yanes3 <cye13766@hotmail.com>
2024-04-07 23:26:44 -0500

    Se realiza correcion a botones y mejora en la navbar

--- src/util/MyButton.js
+++ src/util/MyButton.js
@@ -1,11 +1,11 @@
-import React from 'react' ;
+import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 
-export default({ children, OnClick , tip , btnClassName , tipClassName }) => (
-    <Tooltip title={tip} className={tipClassName} placement="top">
-        <IconButton onClick={OnClick} className={btnClassName} >
-            {children}
-        </IconButton>
-     </Tooltip>   
-)
+export default ({ children, onClick, tip, btnClassName, tipClassName }) => (
+  <Tooltip title={tip} className={tipClassName} placement="top">
+    <IconButton onClick={onClick} className={btnClassName}>
+      {children}
+    </IconButton>
+  </Tooltip>
+);
